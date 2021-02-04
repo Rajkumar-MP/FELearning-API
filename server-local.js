b392eec749c8d8a91916b93f5cddce401db287b5
Rajkumar.MP <mprajcs@gmail.com>
2021-02-04 18:12:43 +0530

    fix: json

--- server-local.js
+++ server-local.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const {app, router} = require('./api/server');
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use('/',router);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
