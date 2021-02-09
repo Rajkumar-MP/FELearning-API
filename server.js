d98958d2928cadec2816561f19523e93c69573a4
Rajkumar.MP <mprajcs@gmail.com>
2021-02-09 14:05:04 +0530

    feat: heroku

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const {app, router} = require('./api/server');
+const app = require('./src/api');
 const port = process.env.PORT || 3000;
-app.use('/',router);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
