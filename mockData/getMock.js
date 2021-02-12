fee1a661132a62699fef5d5277ab3fc51036a939
Rajkumar.MP <mprajcs@gmail.com>
2021-02-12 19:02:14 +0530

    feat: add new api

--- mockData/getMock.js
+++ mockData/getMock.js
@@ -1,8 +1,12 @@
 const aadharInfo = require('./mock/aadhar.json');
 const laborActivities = require('./mock/labor-activities.json');
+const accountInfo = require('./mock/accountInfo.json');
+const login = require('./mock/login.json');
 
 
 module.exports = {
    ...aadharInfo,
-   ...laborActivities 
+   ...laborActivities,
+   ...accountInfo,
+   ...login
 };
--- src/api.js
+++ src/api.js
@@ -2,16 +2,29 @@ const express = require("express");
 const app = express();
 const path = require('path');
 
+const bodyParser = require('body-parser');
+
 const mockJson = require('../mockData/getMock');
 
 const getSpecificData = (obj, id) => obj.find(val => val.id === id) || {};
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.get("/aadhar/:id", (req, res) => { res.json(getSpecificData(mockJson['aadhar'], req.params.id)); });
+app.get("/accountinfo/:id", (req, res) => { res.json(getSpecificData(mockJson['accountinfo'], req.params.id)); });
 app.get(`/laboral-activities`, (req, res) => res.json({ route: mockJson['laboral-activities'] }));
 app.get(`/expected-turnover`, (req, res) => res.json({ route: mockJson['expected-turnover'] }));
+app.post(`/login`, (req, res) => {
+    const { username, password } = req.body || {};
+    const  { login } = mockJson;
+    const response = login.find(val => val.username === username && val.password === password) || {};
+    return res.json(response);
+
+});
+
 
 module.exports = app;
